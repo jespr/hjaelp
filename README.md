09eb365f7ed4cdc6bff44585ec01032d4d5b38ed
Jesper Christiansen <jchristiansen@zendesk.com>
2014-06-03 21:22:46 +0200

    Specify placement using data-placement

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 Hjælp (hjaelp) means Help in danish. This plugin is intended for adding small
 help icons on your site, which will reveal a small descriptive pop-over on click.
 
-## Example
+## Examples
 
 ```html
 <div class="help" data-title="Help" data-content="A helpful text" data-icon="?"></div>
@@ -14,3 +14,7 @@ help icons on your site, which will reveal a small descriptive pop-over on click
 ```javascript
 $('.help').hjaelp();
 ```
+
+### Placement
+
+You can chose to have the majority of the popover on the right side of the icon (default) - or on the left side of the icon. This can be done using the ``data-placement`` attribute on the element. The valid options are ``right`` (default) and ``left``.
--- hjaelp.js
+++ hjaelp.js
@@ -6,7 +6,7 @@
  * Project home: https://github.com/jespr/hjaelp
  * Licensed under the Apache license
  *
- * Version: 1.0
+ * Version: 1.1.0
  */
 
 ;(function($) {
@@ -14,7 +14,9 @@
   var defaults = {
     icon: "?",
     title: "Title",
-    showArrow: true
+    showArrow: true,
+    iconWidth: 20,
+    offsetMargin: 10
   };
 
   function Hjaelp(targets, options) {
@@ -24,7 +26,8 @@
       var element = $(this),
           data = {
             title: element.data('title'),
-            icon: element.data('icon')
+            icon: element.data('icon'),
+            placement: element.data('placement')
           };
 
       that.options = $.extend({}, defaults, options, data);
@@ -41,7 +44,8 @@
           arrow   = "<div class='hjaelp-popover-arrow'></div>",
           title   = "<div class='title'>" + this.options.title + "</div>",
           content = "<p>" + element.html() + "</p>",
-          popover = $(popoverContainer);
+          popover = $(popoverContainer),
+          margin  = -this.options.offsetMargin;
 
       if (this.options.width) {
         popover.width(this.options.width);
@@ -55,6 +59,15 @@
 
       element.html("<a class='hjaelp-icon'>" + this.options.icon + "</a>");
       element.append(popover);
+
+      if (this.options.placement === 'left') {
+        margin = -(popover.width() - this.options.iconWidth - this.options.offsetMargin);
+        popover.addClass('hjaelp-left');
+      } else {
+        popover.addClass('hjaelp-right');
+      }
+
+      popover.css({ 'margin-left': margin + 'px'});
     },
 
     registerEventhandlers: function(element) {
